2c2edca5eea65531443032d56649252d3c34bf88
bk-git-hub <bkprivate99@gmail.com>
2025-03-16 13:18:03 +0900

    feat: add SpeechBubble component

--- src/components/ChatDisplay.tsx
+++ src/components/ChatDisplay.tsx
@@ -1,3 +1,10 @@
+import SpeechBubble from './SpeechBubble';
+
 export default function ChatDisplay() {
-  return <div className='w-full h-[30%] bg-green-200'>채팅</div>;
+  return (
+    <div className='w-full h-[30%] bg-green-200'>
+      <SpeechBubble position='right'>예시 질문</SpeechBubble>
+      <SpeechBubble> 예시 답변</SpeechBubble>
+    </div>
+  );
 }
