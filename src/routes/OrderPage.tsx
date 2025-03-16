4c2817e9f58edb065e03dbbfcf2be8ab370b412c
bk-git-hub <bkprivate99@gmail.com>
2025-03-16 15:45:51 +0900

    chore: add border in OrderPage

--- src/routes/OrderPage.tsx
+++ src/routes/OrderPage.tsx
@@ -5,7 +5,7 @@ import MenuDisplay from '../components/MenuDisplay';
 export default function OrderPage() {
   return (
     <div className='flex w-screen h-screen'>
-      <div className='w-[70%] h-full flex flex-col'>
+      <div className='w-[70%] h-full flex flex-col border-r-8 border-white'>
         <MenuDisplay />
         <ChatDisplay />
       </div>
