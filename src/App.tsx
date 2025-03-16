c669b8253420ca7b0171e1b0a2fbfdea44cc9135
bk-git-hub <bkprivate99@gmail.com>
2025-03-16 12:51:46 +0900

    feat: add OrderPage route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import OrderPage from './routes/OrderPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path='order' element={<OrderPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
