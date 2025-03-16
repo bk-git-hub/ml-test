64a77a4ce9af596578189521d1a9325e77448120
bk-git-hub <bkprivate99@gmail.com>
2025-03-16 13:03:35 +0900

    feat: add CartContainer component

---
+++ src/components/CartContainer.tsx
@@ -0,0 +1,7 @@
+export default function CartContainer() {
+  return (
+    <div className='w-[30%] bg-red-100 flex flex-col items-center py-5'>
+      <h2 className='text-3xl'>장바구니</h2>
+    </div>
+  );
+}
--- src/routes/OrderPage.tsx
+++ src/routes/OrderPage.tsx
@@ -1,9 +1,11 @@
+import CartContainer from '../components/CartContainer';
 import MenuDisplay from '../components/MenuDisplay';
 
 export default function OrderPage() {
   return (
     <div className='flex w-screen h-screen'>
       <MenuDisplay />
+      <CartContainer />
     </div>
   );
 }
