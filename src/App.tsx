83bdc89b399abfccfa56810ee30cfa57adb60a83
bk-git-hub <bkprivate99@gmail.com>
2025-03-16 15:44:14 +0900

    feat: add checkout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import OrderPage from './routes/OrderPage';
 import HomePage from './routes/HomePage';
+import CheckoutPage from './routes/CheckoutPage';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='order' element={<OrderPage />} />
+        <Route path='checkout' element={<CheckoutPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/CartContainer.tsx
+++ src/components/CartContainer.tsx
@@ -8,7 +8,7 @@ export default function CartContainer() {
   );
 
   return (
-    <div className='w-full h-full bg-red-100 flex flex-col'>
+    <div className='w-full h-full bg-gray-300 flex flex-col'>
       <h2 className='text-3xl w-full text-center p-4'>장바구니</h2>
 
       <ul className='grow'>
@@ -21,7 +21,12 @@ export default function CartContainer() {
       <div className='w-full mt-5 text-xl font-bold p-4'>
         총 가격: ₩{totalPrice.toLocaleString()}
       </div>
-      <button className='text-xl bg-red-500 text-white p-5'>주문하기</button>
+      <a
+        href='/checkout'
+        className='text-3xl  bg-red-500 text-white p-5 text-center'
+      >
+        주문하기
+      </a>
     </div>
   );
 }
