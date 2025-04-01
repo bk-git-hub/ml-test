ab5c6149cd318a02088bba04ed628b8eb39c7a5c
bk-git-hub <bkprivate99@gmail.com>
2025-04-01 16:33:56 +0900

    chore: add mock data for navigation bar component

---
+++ src/mocks/categories.ts
@@ -0,0 +1,15 @@
+// src/mocks/categories.ts
+
+import { Category } from '../types/order';
+
+export const mockCategories: Category[] = [
+  { category_id: 1, category_name: '식사류' },
+  { category_id: 2, category_name: '음료' },
+  { category_id: 3, category_name: '디저트' },
+  { category_id: 4, category_name: '스낵' },
+  { category_id: 5, category_name: '사이드 메뉴' },
+  { category_id: 6, category_name: '식사류2' },
+  { category_id: 7, category_name: '음료2' },
+  { category_id: 8, category_name: '디저트2' },
+  { category_id: 9, category_name: '스낵2' },
+];
