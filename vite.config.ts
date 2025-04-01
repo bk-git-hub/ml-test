4fa5093cbd64ee27f84af85171b7390c22787e03
bk-git-hub <bkprivate99@gmail.com>
2025-04-01 16:28:39 +0900

    feat: Alias src directory to @ for cleaner imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path'; // Import the 'path' module
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+    ],
+  },
 });
