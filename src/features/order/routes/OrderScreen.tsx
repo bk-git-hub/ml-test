94c6ce16af2b0ba586e92d543c6f110a16d56356
bk-git-hub <bkprivate99@gmail.com>
2025-04-01 16:10:38 +0900

    feat: add order screen component

---
+++ src/features/order/routes/OrderScreen.tsx
@@ -0,0 +1,9 @@
+// src/features/order/routes/OrderScreen.tsx
+
+import React from 'react';
+
+const OrderScreen: React.FC = () => {
+  return <div className='flex h-screen'></div>;
+};
+
+export default OrderScreen;
