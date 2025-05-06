330add73389fa5c9f41ef359c0d6befd08cc81bc
bk-git-hub <bkprivate99@gmail.com>
2025-05-07 00:32:32 +0900

    포기

--- src/features/order/components/KeywordDetector.tsx
+++ src/features/order/components/KeywordDetector.tsx
@@ -2,12 +2,10 @@ import { useEffect, useState } from 'react';
 import { usePorcupine } from '@picovoice/porcupine-react';
 import VoiceRecorder from './VoiceRecorder';
 
-const PORCUPINE_MODEL_PATH = `${
-  import.meta.env.BASE_URL
-}pp/porcupine_params_ko.pv`;
-const PORCUPINE_KEYWORD_PATH = `${
-  import.meta.env.BASE_URL
-}pp/말랑아_ko_wasm_v3_0_0.ppn`;
+const BASE_URL = 'https://mallang-test.vercel.app/';
+
+const PORCUPINE_MODEL_PATH = `${BASE_URL}pp/porcupine_params_ko.pv`;
+const PORCUPINE_KEYWORD_PATH = `${BASE_URL}pp/mallanga_ko_wasm_v3_0_0.ppn`;
 const PORCUPINE_ACCESS_KEY = import.meta.env.VITE_PORCUPINE_ACCESS_KEY;
 
 const porcupineKeyword = {
