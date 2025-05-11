5834519f6c59a1044b58ab9deeef3c34769f04e3
bk-git-hub <bkprivate99@gmail.com>
2025-05-11 17:48:10 +0900

    feat: create zustand store for chat message

---
+++ src/features/chat/store/chatStore.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand';
+
+export interface Message {
+  text: string;
+  isUser: boolean;
+  timestamp: number;
+}
+
+interface ChatStore {
+  messages: Message[];
+  addMessage: (message: Message) => void;
+  clearMessages: () => void;
+}
+
+export const useChatStore = create<ChatStore>((set) => ({
+  messages: [],
+  addMessage: (message) =>
+    set((state) => ({
+      messages: [...state.messages, message],
+    })),
+  clearMessages: () => set({ messages: [] }),
+}));
