59115e7a7b8a47fa6c95162123af9ff7e560ad71
bk-git-hub <bkprivate99@gmail.com>
2025-05-12 16:18:01 +0900

    chore: update Menu type interface

--- src/types/menu.ts
+++ src/types/menu.ts
@@ -2,10 +2,8 @@ export interface Menu {
   id: number;
   name: string;
   price: number;
-  description: string;
   imageUrl?: string;
   categoryId: number;
-  isAvailable: boolean;
 }
 
 export interface Category {
