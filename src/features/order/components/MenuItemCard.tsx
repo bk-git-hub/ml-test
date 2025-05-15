7271a0a9be951c0b28e730840454964a7323520e
bk-git-hub <bkprivate99@gmail.com>
2025-05-16 00:19:31 +0900

    style: add selected effect for searched menu card

--- src/features/order/components/MenuItemCard.tsx
+++ src/features/order/components/MenuItemCard.tsx
@@ -4,9 +4,10 @@ import { useCartStore } from '@/store/cartStore';
 
 interface MenuItemCardProps {
   menu: Menu;
+  isSearched: boolean;
 }
 
-const MenuItemCard = ({ menu }: MenuItemCardProps) => {
+const MenuItemCard = ({ menu, isSearched }: MenuItemCardProps) => {
   const addItem = useCartStore((state) => state.addItem);
 
   const handleAddToCart = () => {
@@ -15,12 +16,20 @@ const MenuItemCard = ({ menu }: MenuItemCardProps) => {
   };
 
   return (
-    <div className='bg-ml-yellow rounded-lg shadow hover:shadow-md transition-shadow duration-200 ease-in-out overflow-hidden flex flex-col p-4 gap-4 items-center'>
+    <div
+      className={`bg-ml-yellow rounded-lg shadow hover:shadow-md transition-shadow duration-200 ease-in-out overflow-hidden flex flex-col p-4 gap-4 items-center ${
+        isSearched
+          ? 'animate-[pulse_1s_ease-in-out_5] border-2 border-ml-yellow-light ring-2 ring-ml-yellow-light'
+          : ''
+      }`}
+    >
       {/* Image */}
       <img
         src={menu.imageUrl}
         alt={menu.name}
-        className='w-[80%] object-cover'
+        className={`w-[80%] object-cover ${
+          isSearched ? 'animate-[pulse_1s_ease-in-out_5]' : ''
+        }`}
         onError={(e) => {
           const target = e.target as HTMLImageElement;
           target.src = '/logo.png';
@@ -29,23 +38,30 @@ const MenuItemCard = ({ menu }: MenuItemCardProps) => {
 
       {/* Content Area */}
       <div className='flex flex-col flex-grow w-full'>
-        {' '}
-        {/* Use flex-grow to push button down */}
         {/* Name and Price */}
         <div className='flex-grow mb-3'>
-          {' '}
-          {/* Use flex-grow here too */}
-          <h3 className='font-semibold text-md mb-1 truncate' title={menu.name}>
+          <h3
+            className={`font-semibold text-md mb-1 truncate ${
+              isSearched ? 'animate-[pulse_1s_ease-in-out_5]' : ''
+            }`}
+            title={menu.name}
+          >
             {menu.name}
           </h3>
-          <p className='text-sm text-gray-700'>
+          <p
+            className={`text-sm text-gray-700 ${
+              isSearched ? 'animate-[pulse_1s_ease-in-out_5]' : ''
+            }`}
+          >
             {menu.price.toLocaleString()}원
           </p>
         </div>
         {/* Add to Cart Button */}
         <button
           onClick={handleAddToCart}
-          className='mt-auto w-full bg-white hover:bg-blue-600  font-semibold py-2 px-3 rounded text-sm transition-colors duration-150 ease-in-out' // Added mt-auto
+          className={`mt-auto w-full bg-white hover:brightness-110 font-semibold py-2 px-3 rounded text-sm transition-colors duration-150 ease-in-out ${
+            isSearched ? 'animate-[pulse_1s_ease-in-out_5]' : ''
+          }`}
         >
           담기
         </button>
