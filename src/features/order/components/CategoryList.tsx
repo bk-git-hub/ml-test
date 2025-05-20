235537daade6134a8bc293b5084113a6a11e4708
bk-git-hub <bkprivate99@gmail.com>
2025-05-20 18:43:27 +0900

    refactor: change types to render

--- src/features/order/components/CategoryList.tsx
+++ src/features/order/components/CategoryList.tsx
@@ -14,6 +14,10 @@ const CategoryList = () => {
   const { categories } = useMenuStore();
   const { language } = useLanguageStore(); // 'ko' 또는 'en'
 
+  const filteredCategories = categories.filter(
+    (category) => category.categoryName !== '전체'
+  );
+
   if (!currentCategoryId) {
     initializeCategory();
   }
@@ -28,25 +32,27 @@ const CategoryList = () => {
   };
 
   return (
-    <div className="flex">
-      <aside className="bg-slate-800 rounded-xl shadow-lg p-4 mt-4 mb-6 ml-4 w-40 flex flex-col select-none">
+    <div className='flex'>
+      <aside className='bg-slate-800 rounded-xl shadow-lg p-4 mt-4 mb-6 ml-4 w-40 flex flex-col select-none'>
         {/* 카테고리 목록 */}
-        <nav className="flex-1 overflow-y-auto scrollbar-thin scrollbar-thumb-indigo-400 scrollbar-track-indigo-900/30">
-          <ul className="flex flex-col gap-2">
-            {categories.map((category) => {
+        <nav className='flex-1 overflow-y-auto scrollbar-thin scrollbar-thumb-indigo-400 scrollbar-track-indigo-900/30'>
+          <ul className='flex flex-col gap-2'>
+            {filteredCategories.map((category) => {
               const isActive =
                 !isNaN(currentCategoryId) &&
                 currentView === 'menu' &&
-                category.category_id === currentCategoryId;
+                category.categoryId === currentCategoryId;
 
               // 언어에 따라 이름 선택
               const categoryName =
-                language === 'en' ? category.category_name_en : category.category_name;
+                language === 'en'
+                  ? category.categoryNameEn
+                  : category.categoryName;
 
               return (
-                <li key={category.category_id}>
+                <li key={category.categoryId}>
                   <button
-                    onClick={() => handleCategoryClick(category.category_id)}
+                    onClick={() => handleCategoryClick(category.categoryId)}
                     className={clsx(
                       'w-full py-3 rounded-lg text-center transition-colors duration-300 ease-in-out text-sm font-semibold',
                       isActive
@@ -63,12 +69,13 @@ const CategoryList = () => {
         </nav>
 
         {/* 주문내역조회 버튼 */}
-        <div className="w-full mt-6">
+        <div className='w-full mt-6'>
           <button
             onClick={handleOrderHistoryClick}
             className={clsx(
               'w-full font-semibold py-3 rounded-md text-sm bg-indigo-100 text-indigo-900 transition duration-200 ease-in-out hover:brightness-90 shadow-sm',
-              currentView === 'orderHistory' && 'shadow-inner ring-2 ring-indigo-500'
+              currentView === 'orderHistory' &&
+                'shadow-inner ring-2 ring-indigo-500'
             )}
           >
             {language === 'en' ? 'Order History' : '주문 내역 조회'}
